55d05477a8c08dcea59771aaa06053a6da236572
Nazanin Hashemian <69569381+NazaninHashemian@users.noreply.github.com>
2025-10-08 20:17:22 -0700

    Ready for deployment

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -1,6 +1,12 @@
+import animationImg from "../assets/animation.jpg";
 function About() {
   return (
     <section id="about" className="about-section">
+      <img
+        src={animationImg}
+        alt="Animation"
+        style={{ width: "6%", maxHeight: "300px", objectFit: "cover" }}
+      />
       <h2>About Us</h2>
       <p>Experience the best haircut services in Coquitlam.</p>
       <p>3320 Rosalie Crt, Coquitlam, British Columbia, Canada V3E 2Z8</p>
